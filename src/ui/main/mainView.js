3a7b72a4252dabeacd512e9821717463b51dd7d0
Kharlamov Dmitry <newdimaak@gmail.com>
2021-08-06 14:00:06 +0300

    Added 2 signs to the time string

--- src/ui/main/mainView.js
+++ src/ui/main/mainView.js
@@ -37,8 +37,8 @@ export default class MainView {
 
   getCurrentTimeString() {
     const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
+    const hours = String(date.getHours()).padStart(2, 0);
+    const minutes = String(date.getMinutes()).padStart(2, 0);
 
     return `${hours}:${minutes}`;
   }
